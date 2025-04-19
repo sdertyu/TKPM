776e9d246a51534b79ddb1a5cde6c7cb31469e6a
PNgo <114852769+sdertyu@users.noreply.github.com>
2025-04-20 02:30:08 +0700

    them csdl

---
+++ backend/tkpm.sql
@@ -0,0 +1,17 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : tkpm
+ Source Server Type    : PostgreSQL
+ Source Server Version : 170004 (170004)
+ Source Host           : localhost:2345
+ Source Catalog        : tkpm
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 170004 (170004)
+ File Encoding         : 65001
+
+ Date: 20/04/2025 02:29:49
+*/
+
